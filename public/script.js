d373a62edfba297577fd6093ba2698b6c3c35332
faithkelsi <josephfaith307@gmail.com>
2026-02-07 11:31:35 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,33 @@
+const pathParts = window.location.pathname.split("/");
+const userId = pathParts[pathParts.length - 1];
+
+// Send message
+function sendMessage() {
+  const message = document.getElementById("message").value;
+
+  fetch(`/send/${userId}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message })
+  })
+    .then(res => res.json())
+    .then(() => {
+      document.getElementById("status").innerText = "Message sent anonymously ✔️";
+      document.getElementById("message").value = "";
+    });
+}
+
+// Load inbox
+if (window.location.pathname.includes("/inbox")) {
+  fetch(`/messages/${userId}`)
+    .then(res => res.json())
+    .then(data => {
+      const ul = document.getElementById("messages");
+      data.forEach(msg => {
+        const li = document.createElement("li");
+        li.innerText = msg.text;
+        ul.appendChild(li);
+      });
+    });
+}
 
